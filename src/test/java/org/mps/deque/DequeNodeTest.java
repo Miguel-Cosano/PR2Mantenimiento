8b1308a938fdc9255b1ec1e8606ab25bf6e7565a
im-somnus <somnusanimus@gmail.com>
2023-03-09 23:18:27 +0100

    Added the tests to my branch, they have 100% coverage to DequeNode

--- src/test/java/org/mps/deque/DequeNodeTest.java
+++ src/test/java/org/mps/deque/DequeNodeTest.java
@@ -1,6 +1,164 @@
 package org.mps.deque;
 
+import org.junit.jupiter.api.*;
 
-public class DequeNodeTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+
+@DisplayName("A Node")
+public class DequeNodeTest
+{
+    DequeNode<Integer> head, node, tail;
+
+    /**
+     * Create the node and the previous and next node for it.
+     */
+    @BeforeEach
+    void setUp()
+    {
+        head = new DequeNode<>(0, null, null);
+        node = new DequeNode<>(1, null, null);
+        tail = new DequeNode<>(2, null, null);
+
+        head.setNext(node);
+        node.setPrevious(head);
+        node.setNext(tail);
+        tail.setPrevious(node);
+    }
+
+    /**
+     * We destroy everything after each test.
+     */
+    @AfterEach
+    void tearDown()
+    {
+        // Destroy the deque nodes
+        node = null;
+        head = null;
+        tail = null;
+    }
+
+    @Nested
+    @DisplayName("when new")
+    class whenNewTests
+    {
+
+        @Nested
+        @DisplayName("head")
+        class headDequeNodeTests
+        {
+            @Test
+            @DisplayName("is not null")
+            void testHeadNodeIsNotNullReturnsTrue()
+            {
+                assertNotNull(head);
+            }
+
+            @Test
+            @DisplayName("is the first node")
+            void testHeadNodeIsFirstNodeReturnsTrue()
+            {
+                boolean expected = true;
+                boolean actual = head.isFirstNode();
+                assertEquals(expected, actual);
+            }
+
+            @Test
+            @DisplayName("is a terminal node")
+            void testHeadNodeIsNotATerminalNodeReturnsFalse()
+            {
+                boolean expected = false;
+                boolean actual = head.isNotATerminalNode();
+                assertEquals(expected, actual);
+            }
+
+            @Test
+            @DisplayName("is not the last node")
+            void testHeadNodeIsTheLastNodeReturnsFalse()
+            {
+                boolean expected = false;
+                boolean actual = head.isLastNode();
+                assertEquals(expected, actual);
+            }
+        }
+
+        @Nested
+        @DisplayName("node")
+        class nodeDequeNodeTests
+        {
+            @Test
+            @DisplayName("is not null")
+            void testNodeIsNotNullReturnsTrue()
+            {
+                assertNotNull(node);
+            }
+
+            @Test
+            @DisplayName("is not the first node")
+            void testNodeIsFirstNodeReturnsFalse()
+            {
+                boolean expected = false;
+                boolean actual = node.isFirstNode();
+                assertEquals(expected, actual);
+            }
+
+            @Test
+            @DisplayName("is not a terminal node")
+            void testNodeIsNotATerminalNodeReturnsTrue()
+            {
+                boolean expected = true;
+                boolean actual = node.isNotATerminalNode();
+                assertEquals(expected, actual);
+            }
+
+            @Test
+            @DisplayName("is not the last node")
+            void testNodeIsTheLastNodeReturnsFalse()
+            {
+                boolean expected = false;
+                boolean actual = node.isLastNode();
+                assertEquals(expected, actual);
+            }
+        }
+
+        @Nested
+        @DisplayName("tail")
+        class tailDequeNodeTests
+        {
+            @Test
+            @DisplayName("is not null")
+            void testTailNodeIsNotNullReturnsTrue()
+            {
+                assertNotNull(tail);
+            }
+
+            @Test
+            @DisplayName("is not the first node")
+            void testTailNodeIsFirstNodeReturnsFalse()
+            {
+                boolean expected = false;
+                boolean actual = tail.isFirstNode();
+                assertEquals(expected, actual);
+            }
+
+            @Test
+            @DisplayName("is a terminal node")
+            void testTailNodeIsNotATerminalNodeReturnsFalse()
+            {
+                boolean expected = false;
+                boolean actual = tail.isNotATerminalNode();
+                assertEquals(expected, actual);
+            }
+
+            @Test
+            @DisplayName("is the last node")
+            void testTailNodeIsTheLastNodeReturnsTrue()
+            {
+                boolean expected = true;
+                boolean actual = tail.isLastNode();
+                assertEquals(expected, actual);
+            }
+        }
+    }
 }
