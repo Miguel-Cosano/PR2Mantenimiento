27eb31cacc155680eb9460d84b972634ecf40928
Miguel Cosano <miguecosano2001@uma.es>
2023-03-10 20:29:05 +0100

    Added DisplayName to Test

--- src/test/java/org/mps/deque/DoublyLinkedListDequeTest.java
+++ src/test/java/org/mps/deque/DoublyLinkedListDequeTest.java
@@ -6,13 +6,21 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@DisplayName("Test DoublyLinkedListDeque")
+@DisplayName("A DoublyLinkedListDeque")
 public class DoublyLinkedListDequeTest {
     DoublyLinkedListDeque<Integer> deque;
+
+    /**
+     * Create a new empty list before each test.
+     */
     @BeforeEach
     public void setUp() {
         deque = new DoublyLinkedListDeque<>();
     }
+
+    /**
+     * Set the list to null after each test.
+     */
 
     @AfterEach
     public void tearDown() {
@@ -22,11 +30,13 @@ public class DoublyLinkedListDequeTest {
     @DisplayName("Basics tests cases")
     class EnqueueTestBasic{
         @Test
+        @DisplayName("Empty list size is 0")
         public void testEmptyList() {
             assertEquals(0, deque.size());
         }
 
         @Test
+        @DisplayName("Prepend with empty list")
         public void testPrependEmptyList() {
             deque.prepend(1);
             assertEquals(1, deque.size());
@@ -35,6 +45,7 @@ public class DoublyLinkedListDequeTest {
         }
 
         @Test
+        @DisplayName("Append with empty list")
         public void testAppendEmptyList() {
             deque.append(1);
             assertEquals(1, deque.size());
@@ -43,6 +54,7 @@ public class DoublyLinkedListDequeTest {
         }
 
         @Test
+        @DisplayName("Basic append")
         public void testPrepend() {
             deque.append(1);
             deque.prepend(2);
@@ -52,6 +64,7 @@ public class DoublyLinkedListDequeTest {
         }
 
         @Test
+        @DisplayName("Delete first node ")
         public void testDeleteFirst() {
             deque.append(1);
             deque.append(2);
@@ -62,6 +75,7 @@ public class DoublyLinkedListDequeTest {
         }
 
         @Test
+        @DisplayName("Delete last node ")
         public void testDeleteLast() {
             deque.append(1);
             deque.append(2);
@@ -76,21 +90,25 @@ public class DoublyLinkedListDequeTest {
     @DisplayName("Exceptions tests cases")
     class EnqueueTestExceptions{
         @Test
+        @DisplayName("Exception when prepend with null value")
         public void testPrependWithNullValue() {
             assertThrows(DoubleEndedQueueException.class, () -> deque.prepend(null));
         }
 
         @Test
+        @DisplayName("Exception when append with null value")
         public void testAppendWithNullValue() {
             assertThrows(DoubleEndedQueueException.class, () -> deque.append(null));
         }
 
         @Test
+        @DisplayName("Exception when delete first with empty list")
         public void testDeleteFirstWithEmptyList() {
             assertThrows(DoubleEndedQueueException.class, () -> deque.deleteFirst());
         }
 
         @Test
+        @DisplayName("Exception when delete last with empty list")
         public void testDeleteLastWithEmptyList() {
             assertThrows(DoubleEndedQueueException.class, () -> deque.deleteLast());
         }
