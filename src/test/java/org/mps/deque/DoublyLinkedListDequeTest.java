e62a5ea70a717557f8d6be8db65465b1aea7ede4
im-somnus <somnusanimus@gmail.com>
2023-03-17 17:44:28 +0100

    updating with my current progress.

--- src/test/java/org/mps/deque/DoublyLinkedListDequeTest.java
+++ src/test/java/org/mps/deque/DoublyLinkedListDequeTest.java
@@ -10,7 +10,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  * author: Edgar Antonio Álvarez González and Miguel Angel Cosano Ramirez
  */
 @DisplayName("A DoublyLinkedListDeque")
-public class DoublyLinkedListDequeTest {
+public class DoublyLinkedListDequeTest
+{
     DoublyLinkedListDeque<Integer> deque;
 
     /**
@@ -19,29 +20,64 @@ public class DoublyLinkedListDequeTest {
     @BeforeEach
     @Test
     @DisplayName("is instantiated with new DoublyLinkedListDeque()")
-    public void setUp() {
+    public void setUp()
+    {
         deque = new DoublyLinkedListDeque<>();
     }
 
-    /**
-     * Set the list to null after each test.
-     */
-
+    @AfterEach
+    public void tearDown()
+    {
+        deque = null;
+    }
 
     @Nested
     @DisplayName("When new")
-    class whenNewTestCases{
+    class whenNewTestCases
+    {
         @Test
         @DisplayName("empty list size is 0")
-        public void testEmptyList() {
+        public void testEmptyList()
+        {
             assertEquals(0, deque.size());
         }
+
+        @Test
+        @DisplayName("throws exception when delete last with empty list")
+        public void testDeleteLastWithEmptyList()
+        {
+            assertThrows(DoubleEndedQueueException.class, () -> deque.deleteLast());
+        }
+
+        @Test
+        @DisplayName("throws exception when delete first with empty list")
+        public void testDeleteFirstWithEmptyList()
+        {
+            assertThrows(DoubleEndedQueueException.class, () -> deque.deleteFirst());
+        }
+
+        @Test
+        @DisplayName("throws exception when append with null value")
+        public void testAppendWithNullValue()
+        {
+            assertThrows(DoubleEndedQueueException.class, () -> deque.append(null));
+        }
+
+        @Test
+        @DisplayName("throws exception when prepend with null value")
+        public void testPrependWithNullValue()
+        {
+            assertThrows(DoubleEndedQueueException.class, () -> deque.prepend(null));
+        }
+
         @Nested
-        @DisplayName("prepend")
-        class AfterPrependTestCases{
+        @DisplayName("prepend()")
+        class AfterPrependTestCases
+        {
             @Test
             @DisplayName("in empty list")
-            public void testPrependEmptyList() {
+            public void testPrependEmptyList()
+            {
                 deque.prepend(1);
                 assertEquals(1, deque.size());
                 assertEquals(1, deque.first());
@@ -50,7 +86,8 @@ public class DoublyLinkedListDequeTest {
 
             @Test
             @DisplayName("in a list with one node")
-            public void testPrepend() {
+            public void testPrepend()
+            {
                 deque.append(1);
                 deque.prepend(2);
                 assertEquals(2, deque.size());
@@ -59,12 +96,15 @@ public class DoublyLinkedListDequeTest {
             }
 
         }
+
         @Nested
-        @DisplayName("append")
-        class AfterAppendTestCases{
+        @DisplayName("append()")
+        class AfterAppendTestCases
+        {
             @Test
             @DisplayName("in empty list")
-            public void testAppendEmptyList() {
+            public void testAppendEmptyList()
+            {
                 deque.append(1);
                 assertEquals(1, deque.size());
                 assertEquals(1, deque.first());
@@ -73,23 +113,24 @@ public class DoublyLinkedListDequeTest {
 
             @Test
             @DisplayName("in a list with one node")
-            public void testAppend() {
+            public void testAppend()
+            {
                 deque.append(1);
                 deque.append(2);
                 assertEquals(2, deque.size());
                 assertEquals(1, deque.first());
                 assertEquals(2, deque.last());
             }
-
-
         }
 
         @Nested
-        @DisplayName("delete")
-        class afterDeleteTestCases{
+        @DisplayName("delete()")
+        class afterDeleteTestCases
+        {
             @Test
             @DisplayName("first node ")
-            public void testDeleteFirst() {
+            public void testDeleteFirst()
+            {
                 deque.append(1);
                 deque.append(2);
                 deque.deleteFirst();
@@ -100,7 +141,8 @@ public class DoublyLinkedListDequeTest {
 
             @Test
             @DisplayName("last node ")
-            public void testDeleteLast() {
+            public void testDeleteLast()
+            {
                 deque.append(1);
                 deque.append(2);
                 deque.deleteLast();
@@ -108,37 +150,224 @@ public class DoublyLinkedListDequeTest {
                 assertEquals(1, deque.first());
                 assertEquals(1, deque.last());
             }
+        }
 
+        /**
+         * Test cases in charge of the get() function.
+         */
+        @Nested
+        @DisplayName("get()")
+        class afterGetANodeFromTheDoublyLinkedListDequeTests
+        {
+            @Test
+            @DisplayName("the empty list throws exception")
+            void testGetWhenTheDoublyLinkedListDequeIsEmptyThrowsIndexOutOfBoundsException()
+            {
+                assertThrows(IndexOutOfBoundsException.class, () -> deque.get(0));
+            }
 
+            @Test
+            @DisplayName("negative index value throws exception")
+            void testGetWhenIndexIsANegativeValueThrowsIndexOutOfBoundsException()
+            {
+                assertThrows(IndexOutOfBoundsException.class, () -> deque.get(-1));
+            }
 
+            @Test
+            @DisplayName("index value is bigger than the size of the deque")
+            void testGetWhenIndexHasAValueBiggerThanDequeSizeThrowsIndexOutOfBoundsException()
+            {
+                deque.append(0);
+                assertThrows(IndexOutOfBoundsException.class, () -> deque.get(1));
+            }
+
+            @Test
+            @DisplayName("the first element of the deque")
+            void testGetTheFirstElementOfTheDequeReturnsTheFirstElement()
+            {
+                deque.append(0);
+                deque.append(1);
+                deque.append(2);
+
+                Integer expected = 0;
+
+                Integer actual = deque.get(0);
+
+                assertEquals(expected, actual);
+            }
+
+            @Test
+            @DisplayName("the last element of the deque")
+            void testGetTheLastElementOfTheDequeReturnsTheFirstElement()
+            {
+                deque.append(0);
+                deque.append(1);
+                deque.append(2);
+
+                Integer expected = 2;
+
+                Integer actual = deque.get(2);
+
+                assertEquals(expected, actual);
+            }
+
+            @Test
+            @DisplayName("any in-between element of the deque")
+            void testGetAnyInBetweenElementsOfTheDequeReturnsTheFirstElement()
+            {
+                deque.append(0);
+                deque.append(1);
+                deque.append(2);
+
+                Integer expected = 1;
+
+                Integer actual = deque.get(1);
+
+                assertEquals(expected, actual);
+            }
 
         }
-        @Test
-        @DisplayName("throws exception when delete last with empty list")
-        public void testDeleteLastWithEmptyList() {
-            assertThrows(DoubleEndedQueueException.class, () -> deque.deleteLast());
-        }
-        @Test
-        @DisplayName("throws exception when delete first with empty list")
-        public void testDeleteFirstWithEmptyList() {
-            assertThrows(DoubleEndedQueueException.class, () -> deque.deleteFirst());
-        }
-        @Test
-        @DisplayName("throws exception when append with null value")
-        public void testAppendWithNullValue() {
-            assertThrows(DoubleEndedQueueException.class, () -> deque.append(null));
-        }
-        @Test
-        @DisplayName("throws exception when prepend with null value")
-        public void testPrependWithNullValue() {
-            assertThrows(DoubleEndedQueueException.class, () -> deque.prepend(null));
-        }
-    }
 
+        /**
+         * Test cases in charge of the contains() function.
+         */
+        @Nested
+        @DisplayName("contains()")
+        class afterContainsANodeInTheDoublyLinkedListDequeTests
+        {
+            @Test
+            @DisplayName(" the value in the queue is present")
+            public void testContainsWhenValueIsPresentReturnsTrue()
+            {
+                deque.append(1);
 
-    @AfterEach
-    public void tearDown() {
-        deque = null;
+                boolean expected = true;
+
+                boolean actual = deque.contains(1);
+
+                assertEquals(expected, actual);
+            }
+
+            @Test
+            @DisplayName(" the value in the queue is not present")
+            public void testContainsWhenValueIsNotPresentReturnsFalse()
+            {
+                deque.append(1);
+
+                boolean expected = false;
+
+                boolean actual = deque.contains(2);
+
+                assertEquals(expected, actual);
+            }
+
+            @Test
+            @DisplayName(" the empty list does not contain any value ")
+            public void testContainsWhenTheDoublyLinkedListDequeIsEmptyReturnFalse()
+            {
+                boolean expected = false;
+
+                boolean actual = deque.contains(2);
+
+                assertEquals(expected, actual);
+            }
+        }
+
+        /**
+         * Test cases in charge of the sort() function.
+         */
+        @Nested
+        @DisplayName("sort()")
+        class afterSortingTheDoublyLinkedListDequeTests
+        {
+
+        }
+
+        /**
+         * Test cases in charge of the remove() function.
+         */
+        @Nested
+        @DisplayName("remove()")
+        class afterRemovingANodeFromTheDoublyLinkedListDequeTests
+        {
+            @Test
+            @DisplayName("from empty list doesnt change the size")
+            public void testRemoveElementInEmptyDequeDoesntChangeDequeSize()
+            {
+                int expected = deque.size();
+
+                deque.remove(0);
+
+                int actual = deque.size();
+
+                assertEquals(expected, actual);
+            }
+
+            @Test
+            @DisplayName("deque size is reduced when deleting an element")
+            public void testRemoveElementInEmptyDequeChangesDequeSize()
+            {
+                deque.append(0);
+                deque.append(1);
+                deque.append(2);
+
+                int expected = 2;
+
+                deque.remove(2);
+
+                int actual = deque.size();
+
+                assertEquals(expected, actual);
+            }
+
+            @Test
+            @DisplayName("deque contains two elements with the same value changes size")
+            public void testRemoveElementWhenTwoElementsWithSameValueChangesSize() {
+                deque.append(0);
+                deque.append(0);
+                deque.append(1);
+
+                Integer expected = 2;
+
+                deque.remove(0);
+
+                Integer actual = deque.size();
+
+                assertEquals(expected, actual);
+
+            }
+
+            @Test
+            @DisplayName("deque contains two elements with the same value removes the first occurrence")
+            public void testRemoveElementWhenTwoElementsWithSameValueRemovesTheFirstOccurrence() {
+                deque.append(0);
+                deque.append(0);
+                deque.append(1);
+
+                Integer expected = 1;
+
+                deque.remove(0);
+
+                Integer actual = deque.get(1);
+
+                assertEquals(expected, actual);
+            }
+
+            @Test
+            @DisplayName("Removing head of the deque returns the new head")
+            void testRemoveHeadOfDequeReturnsTheSecondElementAsTheHead()
+            {
+
+            }
+
+            @Test
+            @DisplayName("Removing head of the deque returns null when only one element")
+            void testRemoveHeadOfDequeReturnsNullWhenNoOtherElementsInQueue()
+            {
+      
+            }
+
+        }
     }
 
 }
